9971e80363033b6f6a7ddfbed9a781321c1bae91
Raya Aliyeva <73crd54@code.edu.az>
2023-04-09 17:42:34 +0400

    task6 and task7 added

--- 1.js/script.js
+++ 1.js/script.js
@@ -6,41 +6,68 @@ console.log(calcAge(22))
 
 // 2// 
 let sum2=0
-for(let i=1;i<10;i++){
-    if(i%2==0){
-        sum2=sum2+i 
+for(let a=1;a<10;a++){
+    if(a%2==0){
+        sum2=sum2+a 
     }
 }
-console.log("Sum of even numbers:",sum2);
+console.log("Sum of even numbers between 0 and 10:",sum2);
 
 
 //3//
  let multiply=1;
-for(i=0;i<17;i++){
-    if(i%2!=0)
-    multiply=multiply*i
+for(m=0;m<17;m++){
+    if(m%2!=0)
+    multiply=multiply*m
 }
-console.log("Multiply of odd number:", multiply);
+console.log("Multiply of odd number between 0 and 17:", multiply);
 
 
 //4// 
 let multiply2=1;
-for(i=10; i <= 18;i++){
-    multiply2=multiply2*i
+for(n=10; n <= 18;n++){
+    multiply2=multiply2*n
 }
-console.log("Multiply of numbers between 10 and 18",multiply2);
+console.log("Multiply of numbers between 10 and 18:",multiply2);
 
 5// 
 let sum5=0;
-for(i=17; i <= 30;i++){
-    if(i==19){
+for(s=17; s <= 30;s++){
+    if(s==19 || s==20){
         continue;
     }
-    if(i==20){
-        continue;
-    }
-    sum5=sum5+i
+    sum5=sum5+s
     
 }
 console.log("Sum of numbers between 17 and 30:",  sum5);
 
+6//
+let i=1;
+let sum6=0;
+while(i<30){
+    if(i%2==0){
+        sum6=sum6+i;
+    }
+    i++;
+}
+    console.log(`Sum of even numbers between 0 and 30:`, sum6);
+
+
+let j=1;
+let sum6odd=0;
+while(j<30){
+    if(j%2!=0){
+        sum6odd+=j;
+    }
+    j++;
+}
+console.log(`Sum of odd number between 0 and 30:`, sum6odd);
+
+console.log(`Subtract of even and odd numbers between 0 and 30:`,sum6odd-sum6);
+ 
+7//
+for(e=1;e<17;e++){
+    if(e%2==0){
+        console.log(` ${e} is even number and  ${e}*${e}=${e**2}`)
+    }
+}
